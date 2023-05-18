b2c8c6f5548ebcc6411bf01a7afbec7aadd9ffc6
joaomacieski <118028484+joaomacieski@users.noreply.github.com>
2023-05-17 22:06:07 -0300

    SITE CERTO

--- js/crud.js
+++ js/crud.js
@@ -8,28 +8,37 @@ window.addEventListener("load", () => {
 })
 
 function atualizar(){
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
     document.querySelector("#tarefas").innerHTML = ""
     tarefas.forEach(tarefa => 
         document.querySelector("#tarefas").innerHTML += criarCard(tarefa))
 }
 
+function filtrar(lista){
+    document.querySelector("#tarefas").innerHTML = ""
+    lista.forEach(tarefa => 
+        document.querySelector("#tarefas").innerHTML += criarCard(tarefa)
+    )
+}
+
 function cadastrar() {
-    const suplemento = document.querySelector("#suplemento").value
+    const suplementos = document.querySelector("#suplementos").value
     const quantidade = document.querySelector("#quantidade").value
     const categoria = document.querySelector("#categoria").value
     const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
     const tarefa = { 
-        suplemento,
+        id: Date.now(),
+        suplementos,
         quantidade,
-        categoria
+        categoria,
+        concluida: false
     }
 
-    if (!isValid(tarefa.suplemento, document.querySelector("#suplemento"))) return
+    if (!isValid(tarefa.suplementos, document.querySelector("#suplementos"))) return
     if (!isValid(tarefa.quantidade, document.querySelector("#quantidade"))) return
 
     tarefas.push(tarefa)
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
 
     atualizar()
     modal.hide()
@@ -48,27 +57,37 @@ function isValid(valor, campo){
 
 }
 
-function apagar(botao){
-    botao.parentNode.parentNode.parentNode.remove()
+function apagar(id){
+    tarefas = tarefas.filter(tarefa=> tarefa.id !== id)
+    atualizar()
+}
+
+function concluir(id){
+    let tarefaEncontrada = 
+            tarefas.find(tarefa => tarefa.id == id)
+    tarefaEncontrada.concluida = true
+    atualizar()
 }
 
 function criarCard(tarefa) {
+    let disabled = tarefa.concluida ? "disabled" : ""
+
     const card = `
         <div class="col-lg-3 col-md-6 col-sm-12">
         <div class="card">
             <div class="card-header">
-                ${tarefa.suplemento}
+                ${tarefa.suplementos}
             </div>
             <div class="card-body">
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">Obrigado por comprar o na melhor loja de suplemento.</p>
                 <p class="card-text">${tarefa.categoria}</p>
-                <span class="badge text-bg-warning">${tarefa.quantidade}pt</span>
+                <span class="badge text-bg-warning">${tarefa.quantidade} unidade(s)</span>
             </div>
             <div class="card-footer">
-                <a href="#" class="btn btn-success" title="marcar como concluída">
+                <a onClick="concluir(${tarefa.id})" href="#" class="btn btn-success ${disabled}" title="marcar como concluída">
                     <i class="bi bi-check2"></i>
                 </a>
-                <a href="#" onClick="apagar(this)" class="btn btn-danger" title="apagar tarefa">
+                <a href="#" onClick="apagar(${tarefa.id})" class="btn btn-danger" title="apagar tarefa">
                     <i class="bi bi-trash3"></i>
                 </a>
             </div> <!-- card footer -->
